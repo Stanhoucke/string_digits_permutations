2e9f13dae2d2e8b623329e61a0299d2f2c7400e8
Stanhoucke <stanhoucke@gmail.com>
2021-04-23 12:07:51 +0100

    solution passed more tests

--- src/test/java/com/and/test/SolutionTest.java
+++ src/test/java/com/and/test/SolutionTest.java
@@ -17,5 +17,13 @@ public class SolutionTest {
         assertEquals("632,623,362,326,263,236", solution.solution("326"));
     }
 
-    
+    @Test
+    public void solutionFor3NumbersWithLettersAndSpaces(){
+        assertEquals("632,623,362,326,263,236", solution.solution("A 3B2 C6D"));
+    }
+
+    @Test
+    public void solutionFor4Numbers(){
+        assertEquals("4321,4312,4231,4213,4132,4123,3421,3412,3241,3214,3142,3124,2431,2413,2341,2314,2143,2134,1432,1423,1342,1324,1243,1234", solution.solution("2143"));
+    }
 }
