4ac8c838503005309cf2842b985c448323d9a909
Stanhoucke <stanhoucke@gmail.com>
2021-04-23 10:44:37 +0100

    psuedocode

--- src/main/java/com/and/test/Solution.java
+++ src/main/java/com/and/test/Solution.java
@@ -11,8 +11,26 @@ public class Solution {
      */
 
     public static String solution(String input) throws NumberFormatException {
+        // Check if input does not contain numbers
+            // Iterate through chars of string
+            // If number found, add to new string
+            // If no numbers found, throw exception
+
+        // Select first digit in number string
+        // Select next digit
+        // repeat until no digits remain (string length is 0)
+        // go to previous digit, replace with last digit
+            // base case, no remaining digits
+            // recursive case, selected digits + remaining digits
+            // add each recursion to results list
+
+        // Sort results in descending order
+        // return string
+
         return null;
     }
+
+    
 
     public static void main(String args[]) {
         solution("326");
